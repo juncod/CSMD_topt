f5ddde2abdfe6480e9d47e4ee539d79631637b9f
JunHyeok_Park <88376704+juncod@users.noreply.github.com>
2022-07-17 19:44:04 +0900

    add sensitivity plot & sensitivity filter

--- 2d_q4/2d_stress(Q4)/Stress_minimize.m
+++ 2d_q4/2d_stress(Q4)/Stress_minimize.m
@@ -2,15 +2,18 @@ function [f0val,df0dx,fval,dfdx,MISES_MAX]=Stress_minimize(NODE,ELEM,x,Hs,H,pl,q
 nele = length(ELEM);
 x(:)=(H*x(:))./Hs;
 [pnorm,pnorm_sen,MISES]=Stress_3D_Sensitivity_Comp(x,NODE,ELEM,pl,q,p);
+
 % Plot Density
-plot_x = max(0,x(:)-0.1);
+plot_x = -ceil(max(0,x(:)-0.1));
 figure(1); colormap(gray);
-patch('Faces',ELEM,'Vertices',NODE,'FaceVertexCData',-x,'FaceColor','flat','LineStyle','none'); axis equal; axis tight; axis off;
-%ceil(x.*-2) or -x
-dv = ones(nele,1)/(nele); 
-sen(:) = H*(pnorm_sen(:)./Hs);
-dv(:) = H*(dv(:)./Hs);
-% sen(:) = H*(x(:).*pnorm_sen(:))./Hs./max(1e-3,x(:)); % => sensitivity filter
+patch('Faces',ELEM,'Vertices',NODE,'FaceVertexCData',-x,'FaceColor','flat','LineStyle','none'); axis equal; axis tight; axis off; caxis([-1 0]);
+figure(2); colormap(jet);
+patch('Faces',ELEM,'Vertices',NODE,'FaceVertexCData',MISES,'FaceColor','flat','LineStyle','none'); axis equal; axis tight; axis off; drawnow; colorbar; caxis([min(MISES) max(MISES)]);
+ 
+sen(:) = (H*pnorm_sen(:))./Hs;
+sen(:) = H*(x(:).*sen(:))./Hs./max(1e-3,x(:));
+dv = ones(nele,1)/(nele);
+dv(:) = (H*dv(:))./Hs;
 fval=[mean(x(:))-volfrac];
 dfdx=[dv(:)'];
 df0dx=sen';
